f702965a7e3012986fba490e7793683d4341079d
Simon_DeBevoise <simondebevoise@gmail.com>
2020-02-12 16:20:42 -0500

    spotify anchor coding challenge

---
+++ spotify_anchor_prompt.js
@@ -0,0 +1,33 @@
+const message = `If you want to jumpstart the process of talking to us about this role, here’s a little challenge: write a program that outputs the largest unique set of characters that can be removed from this paragraph without letting its length drop below 50.`
+
+const largestSubset = (message, minLength = 50) => {
+    if (!message || !message.length) throw Error('Invalid message format');
+    if (message.length < minLength) return [];
+    
+    const counter = {};
+    
+    for (let char of message) {
+        if (char in counter) {
+            counter[char]++;
+        } else {
+            counter[char] = 1;
+        }
+    }
+
+    const sortedChars = Object.keys(counter).sort((a, b) => counter[a] - counter[b]);
+    let { length } = message;
+    const result = [];
+
+    for (let char of sortedChars) {
+        if (length - counter[char] >= minLength) {
+            result.push(char);
+            length = length - counter[char];
+        } else {
+            break;
+        }
+    }
+
+    return result;
+}
+
+console.log(largestSubset(message))
